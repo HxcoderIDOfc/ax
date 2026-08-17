acd7989a9cc32e22f7178c37800e9f5ec0d1c7ab
HxcoderIDOfc <149635285+HxcoderIDOfc@users.noreply.github.com>
2026-08-18 00:34:49 +0700

    Configure native Android system status bar

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './styles.css'
+import { configureNativeUi } from './native.js'
+
+configureNativeUi()
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
