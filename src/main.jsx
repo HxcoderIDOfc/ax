36da2fd7ee13f9367a2ec6055433612b5f8e3f8e
HxcoderIDOfc <149635285+HxcoderIDOfc@users.noreply.github.com>
2026-08-18 07:43:19 +0700

    Load bottom profile and Connectors styling

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import AppV2 from './AppV2.jsx'
 import './styles.css'
+import './nav-connectors.css'
 import { configureNativeUi } from './native.js'
 
 configureNativeUi()
